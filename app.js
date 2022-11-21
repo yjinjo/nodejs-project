a8faa77ac51a2818d6482fe3f457f92c293216d1
yjinjo <yjinjo@berkeley.edu>
2022-11-21 17:59:01 +0900

    #1. 8. 요청 본문 분석 (Stream & Buffer)

--- app.js
+++ app.js
@@ -18,7 +18,24 @@ const server = http.createServer((req, res) => {
   }
 
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    // on 메서드는 특정 이벤트를 들을 수 있도록 한다.
+    // data event는 새 chunk가 읽힐 준비가 될 때 마다 data event가 발생하는 곳에 버퍼가 도움을 준다.
+    // 두 번째 인자로는 모든 data event에 실행될 함수를 넣어야한다.
+    // 그럼 이제 NodeJS는 요청에 대한 모든 data를 얻을 때까지 아래 arrow function이 실행될 것이다.
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      // 위의 chunk들과 상호 작용하기 위해서는 버퍼를 사용해야 한다.
+      // 아래의 코드는 새 버퍼를 생성하고, 본문 안에 있는 모든 chunk가 추가될 것이다.
+      // 그리고 요청의 본문이 text이므로 들어오는 data도 text이기 때문에 문자열로 전환할 수 있다.
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+
     res.statusCode = 302;
     // '/' 로 redirect
     res.setHeader('Location', '/');
