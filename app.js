640fa2338fca675a75a7884986c10a798c0c4530
yjinjo <yjinjo@berkeley.edu>
2022-11-21 17:00:27 +0900

    #1. 4. 요청의 이해

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const http = require("http");
 
 // 1. 서버 생성
 const server = http.createServer((req, res) => {
-  console.log(req);
-  // process.exit();
+  console.log(req.url, req.method, req.headers);
 });
 
 // 2. listen은 Node.js가 스크립트를 바로 종료하지 않고 계속 실행되면서 듣도록 한다.
