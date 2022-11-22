4a0a4c038d0c08197631c36535f634f4953c60ea
yjinjo <yjinjo@berkeley.edu>
2022-11-22 17:35:15 +0900

    #5. 4. Express.js 백그라운드 확인

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+// const http = require('http');
 const express = require('express');
 
 const app = express();
@@ -13,6 +13,8 @@ app.use((req, res, next) => {
   res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000);
+// server.listen(3000);
+
+app.listen(3000);
