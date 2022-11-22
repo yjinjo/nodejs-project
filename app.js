34d4ddfa0a999f3bb4a7ff24cc59b4d343dc61b7
yjinjo <yjinjo@berkeley.edu>
2022-11-22 16:48:39 +0900

    #5. 1. Express.js 설치

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const app = express();
+
+const server = http.createServer();
 
 server.listen(3000);
