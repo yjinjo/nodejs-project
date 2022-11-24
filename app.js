8ec10ce75321f78bd3f01c88ff09e0a535bfb660
yjinjo <yjinjo@berkeley.edu>
2022-11-24 12:58:45 +0900

    #6. 7. Handlebars 작업

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('hbs', expressHbs());
+app.set('view engine', 'hbs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
