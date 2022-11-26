6c371bff9acfdf7c96ef5a1225fe2ee471f06263
yjinjo <yjinjo@berkeley.edu>
2022-11-26 15:07:47 +0900

    #7. 5. 모델을 통해 파일에서 데이터 가져오기

--- models/product.js
+++ models/product.js
@@ -25,7 +25,7 @@ module.exports = class Product {
     });
   }
 
-  static fetchAll() {
+  static fetchAll(cb) {
     const p = path.join(
       path.dirname(process.mainModule.filename),
       'data',
@@ -33,9 +33,9 @@ module.exports = class Product {
     );
     fs.readFile(p, (err, fileContent) => {
       if (err) {
-        return [];
+        cb([]);
       }
-      return JSON.parse(fileContent);
+      cb(JSON.parse(fileContent));
     });
   }
 };
