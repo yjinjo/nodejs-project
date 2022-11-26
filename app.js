859dcc1df14c0ebb1b6667218f6538d3e8cd4896
yjinjo <yjinjo@berkeley.edu>
2022-11-26 11:32:49 +0900

    #6. 10. EJS로 작업

--- app.js
+++ app.js
@@ -2,22 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-  })
-);
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
