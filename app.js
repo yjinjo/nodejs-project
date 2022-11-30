b426bef10ff0927a5de6e5b3e6e48bc74a3164a1
yjinjo <yjinjo@berkeley.edu>
2022-11-30 20:50:56 +0900

    #11. 16. 카트 생성 및 가져오기

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
     return User.findByPk(1);
   })
@@ -59,6 +59,9 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => {
