d8c5f4a56b83fc2686e9908711c09f46a11b5ccc
yjinjo <yjinjo@berkeley.edu>
2022-11-30 15:28:21 +0900

    #11. 1. 데이터베이스에 연결

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'node-complete', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'node-complete',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
