46b69977d04d6bc7171cd8d17bd018c5542dfc1d
yjinjo <yjinjo@berkeley.edu>
2022-11-30 20:05:34 +0900

    #11. 13. 관계 설정 메서드 사용

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    description: description,
-    imageUrl: imageUrl,
-  })
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      description: description,
+      imageUrl: imageUrl,
+    })
     .then((result) => {
       console.log('Created Product');
       res.redirect('/admin/products');
