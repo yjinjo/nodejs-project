5576dc2c96d1e400b92d8323ec5d9b63c03f3618
yjinjo <yjinjo@berkeley.edu>
2022-11-30 17:52:18 +0900

    #11. 4. 데이터 삽입 및 제품 생성

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,14 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
+  Product.create({
+    title: title,
+    price: price,
+    description: description,
+    imageUrl: imageUrl,
+  })
+    .then((result) => {
+      console.log('Created Product');
     })
     .catch((err) => console.log(err));
 };
