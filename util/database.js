5f32d708cdc8bbe2ef8ec7e270ab85c02cdb189f
yjinjo <yjinjo@berkeley.edu>
2022-12-01 14:19:31 +0900

    #12. 03. 데이터베이스 연결 완료

--- util/database.js
+++ util/database.js
@@ -4,17 +4,29 @@ require('dotenv').config();
 const MongoClient = mongodb.MongoClient;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    `mongodb+srv://yjinjo:${MONGO_PASSWORD}@cluster0.olplonm.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://yjinjo:${MONGO_PASSWORD}@cluster0.olplonm.mongodb.net/shop?retryWrites=true&w=majority`
   )
     .then((client) => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
