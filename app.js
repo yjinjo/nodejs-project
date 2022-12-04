235cf1f39b830d139863ba1025f5c922f89d20b9
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-04 12:51:53 +0900

    #12. 17. 장바구니에 담기 기능 추가

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('638c0c2b665f9241e0e4e2b4')
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
