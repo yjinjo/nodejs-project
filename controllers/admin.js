ac96923ba9d55d6a6a183302dd088852e927bbef
yjinjo <yjinjo@berkeley.edu>
2022-12-04 09:52:31 +0900

    #12. 05. 제품 만들기

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,13 +13,9 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user
-    .createProduct({
-      title: title,
-      price: price,
-      description: description,
-      imageUrl: imageUrl,
-    })
+  const product = new Product(title, price, description, imageUrl);
+  product
+    .save()
     .then((result) => {
       console.log('Created Product');
       res.redirect('/admin/products');
