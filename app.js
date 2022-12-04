823509467347506f68043b84fc510c4b2421a4b1
Youngjin Jo <yjinjo@berkeley.edu>
2022-12-04 16:27:30 +0900

    #13. 03. Mongoose를 통한 데이터 저장

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -20,14 +20,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('638c0c2b665f9241e0e4e2b4')
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('638c0c2b665f9241e0e4e2b4')
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,7 +36,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    `mongodb+srv://yjinjo:${MONGO_PASSWORD}@cluster0.olplonm.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://yjinjo:${MONGO_PASSWORD}@cluster0.olplonm.mongodb.net/shop?retryWrites=true&w=majority`
   )
   .then((result) => {
     app.listen(3000);
