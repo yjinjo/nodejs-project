c0f07d6dc35a893594c9b0ec6daed72352f45ed8
yjinjo <yjinjo@berkeley.edu>
2022-12-04 09:45:18 +0900

    #12. 04. 데이터베이스 연결 사용

--- models/product.js
+++ models/product.js
@@ -8,31 +8,18 @@ class Product {
     this.imageUrl = imageUrl;
   }
 
-  save() {}
+  save() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .insertOne(this)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
-
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false,
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
 
 module.exports = Product;
